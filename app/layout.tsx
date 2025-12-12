b471e8ef7997e6cc353f2e143067669ebbf05195
unknown <viddeveloper678@gmail.com>
2025-12-12 14:47:00 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Header from "@/components/Layout/Header";
+import Header from "@/components/layout/Header";
 
 export const metadata = {
   title: "BuyNest – Online Grocery & Essentials",
